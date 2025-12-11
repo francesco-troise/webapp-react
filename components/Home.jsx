ec418501be2a87f2e509decfb5c611c2b09d46e5
FrancescoTroise <francesco.troise98@gmail.com>
2025-12-11 22:28:35 +0100

    correct syntax

--- components/Home.jsx
+++ components/Home.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost:3000/movies/").then((response) => {
@@ -15,31 +15,33 @@ export default function Home() {
       <div className="container mt-5">
         <div className="row g-4">
           {movies.map((movie) => {
-            <div className="col-4" key={movie.id}>
-              <div className="card h-100">
-                <div className="position-relative">
-                  <img
-                    src="poster.jpg"
-                    className="card-img-top"
-                    alt="Poster Film"
-                  />
+            return (
+              <div className="col-4" key={movie.id}>
+                <div className="card h-100">
+                  <div className="position-relative">
+                    <img
+                      src="poster.jpg"
+                      className="card-img-top"
+                      alt="Poster Film"
+                    />
 
-                  <h5 className="position-absolute top-0 start-0 m-2 text-white bg-dark bg-opacity-75 p-1 rounded">
-                    {movie.title}
-                  </h5>
-                </div>
-
-                <div className="card-footer bg-light">
-                  {/* Riga con Regista e Autore */}
-                  <div className="d-flex justify-content-between mb-2">
-                    <span className="fw-bold">Regista</span>
-                    <span className="text-muted">{movie.director}</span>
+                    <h5 className="position-absolute top-0 start-0 m-2 text-white bg-dark bg-opacity-75 p-1 rounded">
+                      {movie.title}
+                    </h5>
                   </div>
 
-                  <p className="card-text">{movie.abstract}</p>
+                  <div className="card-footer bg-light">
+                    {/* Riga con Regista e Autore */}
+                    <div className="d-flex justify-content-between mb-2">
+                      <span className="fw-bold">Regista</span>
+                      <span className="text-muted">{movie.director}</span>
+                    </div>
+
+                    <p className="card-text">{movie.abstract}</p>
+                  </div>
                 </div>
               </div>
-            </div>;
+            );
           })}
         </div>
       </div>
